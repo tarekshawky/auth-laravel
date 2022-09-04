6980d3709560be068056f88c76828c015476ff5d
tarek shawky <tarekshawky2016@gmail.com>
2022-09-04 14:23:30 +0200

    first

---
+++ resources/views/admin/welcome.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Admin Auth Laravel 8 </title>
+</head>
+<body>
+  
+<div class="container">
+    Welcome, Admin
+</div>
+   
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminAuthController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+    Route::get('/login', [AdminAuthController::class, 'getLogin'])->name('adminLogin');
+    Route::post('/login', [AdminAuthController::class, 'postLogin'])->name('adminLoginPost');
+ 
+    Route::group(['middleware' => 'adminauth'], function () {
+        Route::get('/', function () {
+            return view('admin/welcome');
+        })->name('adminDashboard');
+ 
+    });
+});
